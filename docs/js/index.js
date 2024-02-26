1c5b914e680e4650a886a6cf1918bedf88a86410
Logan <logan.ash2025@gmail.com>
2024-02-26 01:55:44 -0500

    initial set up for signed out user

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,24 @@
-//
+// Initialize the map variable
+var map;
+
+// Function to initialize the map
+function initMap() {
+  // Create a new map and place it in the 'map' div
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 36.06865390159834, lng: -79.8107065341785},
+    zoom: 15
+  });
+  
+  // Add moving objects here
+  // You can use the Google Maps API to add markers and move them based on coordinates
+}
+
+// Load the map script dynamically
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAocoRC-ANc3DKCbLq20avhWN4gM8Bf4Ls&callback=initMap';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
